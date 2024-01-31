ad3abbe783d04387736fb163e308e103bdbef2b9
choijunghwan <vkdlxj3562@naver.com>
2024-02-01 00:55:57 +0900

    feat: 데코레이터(decorator) 패턴 예제

---
+++ src/main/java/org/example/decorator/io/InputTest.java
@@ -0,0 +1,21 @@
+package org.example.decorator.io;
+
+import java.io.*;
+
+public class InputTest {
+    public static void main(String[] args) {
+        int c;
+
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/decorator/starbuzz/Whip.java
@@ -0,0 +1,17 @@
+package org.example.decorator.starbuzz;
+
+public class Whip extends CondimentDecorator {
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+}
