5f1a799fae0a39159f3cea9a172e1a798703d470
choijunghwan <vkdlxj3562@naver.com>
2024-02-09 14:41:26 +0900

    feat: 싱글톤(singleton) 패턴 예제

---
+++ src/main/java/org/example/singleton/enumS/Singleton.java
@@ -0,0 +1,9 @@
+package org.example.singleton.enumS;
+
+public enum Singleton {
+    UNIQUE_INSTANCE;
+
+    public String getDescription() {
+        return "I'm a thread safe Singleton!";
+    }
+}
---
+++ src/main/java/org/example/singleton/threadSafe/Singleton.java
@@ -0,0 +1,14 @@
+package org.example.singleton.threadSafe;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
