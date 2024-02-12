058e49c9b1b1c03509b1c065ebf3df5e589dc096
choijunghwan <vkdlxj3562@naver.com>
2024-02-12 13:09:40 +0900

    feat: 어댑터(adapter) 패턴 연습문제

---
+++ src/main/java/org/example/adapter/example/PrintBanner.java
@@ -0,0 +1,19 @@
+package org.example.adapter.example;
+
+public class PrintBanner implements Print {
+    private Banner banner;
+
+    public PrintBanner(String string) {
+        this.banner = new Banner(string);
+    }
+
+    @Override
+    public void printWeak() {
+        banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster();
+    }
+}
---
+++ src/main/java/org/example/adapter/example/PrintTest.java
@@ -0,0 +1,7 @@
+package org.example.adapter.example;
+
+public class PrintTest {
+    public static void main(String[] args) {
+
+    }
+}
