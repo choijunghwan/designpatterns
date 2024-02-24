b3b7920906e019462215559080e0672f1152b323
choijunghwan <vkdlxj3562@naver.com>
2024-02-24 23:28:41 +0900

    feat: 프록시(proxy) 패턴 학습

---
+++ src/main/java/org/example/proxy/gumball/State.java
@@ -0,0 +1,13 @@
+package org.example.proxy.gumball;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+    public void insertQuarter();
+
+    public void ejectQuarter();
+
+    public void turnCrank();
+    public void dispense();
+    public void refill();
+}
